71586c7f573b4a33abaaf4a9df20d38ec5e94c17
MacStudent <macstudent@128-imac23.local>
2018-11-09 09:13:20 -0500

    Add and Load BUtton

--- CoreDataExample/ViewController.swift
+++ CoreDataExample/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+    //MARK: Outlets
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    // MARK: CoreDta variables
+    // ------------------------------
+    var context:NSManagedObjectContext!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World")
+        
+        
+        // Setup your CoreData variable
+        // ----------------------------------------
+        
+        // 1. Mandatory - copy and paste this
+        // Explanation: try to create/initalize the appDelegate variable.
+        // If creation fails, then quit the app
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        // 2. Mandatory - initialize the context variable
+        // This variable gives you access to the CoreData functions
+        self.context = appDelegate.persistentContainer.viewContext
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    
+    //MARK:- Add Actions
+
+    @IBAction func buttonAddPressed(_ sender: UIButton) {
     }
-
-
+    
+    @IBOutlet weak var buttonLoadPressed: UIButton!
 }
 
